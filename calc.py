d8c1c80336f9891b585234b929bd7e0c27605ff3
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2017-12-16 00:40:15 -0200

    calc.py file creation for beginnig to code

---
+++ calc.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+    This is the implementatiom of a simple postfix notation (polish) calculator.
+    It's for fun... for learn to use Vi... to use Git and GitHub a little!
+'''
+
+
