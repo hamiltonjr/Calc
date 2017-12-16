e64cfe562a458bf0c80606eebe212032dd593459
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2017-12-16 01:04:20 -0200

    Implementation of a functional menu

--- calc.py
+++ calc.py
@@ -7,3 +7,32 @@
 '''
 
 
+print('=-'*30)
+print('>>> SIMPLE POLISH NOTATION CALCULATOR <<<')
+print('=-'*30)
+
+while True:
+    print('>>> MENU <<<')
+    print('''
+        1. Type number
+        2. Stack up
+        3. Add
+        4. Subtract
+        5. Multiply
+        6. Divide
+        7. Unstack
+        8. Clear
+        9. Quit
+    ''')
+
+    opt = input('Option: ').strip().upper()
+    while opt not in '123456789':
+        opt = input('Option:').strip().upper()
+
+    if opt == '9':
+        break
+
+
+    
+
+
